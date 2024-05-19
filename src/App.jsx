b0fb8c0e896c20a7d9327b8a12fe650abfe1208c
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-05-19 08:22:36 +0300

    create home section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './App.module.css';
+import Home from './components/Home/Home';
+
+function App() {
+
+  return (
+    <section className={styles.container}>
+      <Home />
+    </section>
+  )
+}
+
+export default App
